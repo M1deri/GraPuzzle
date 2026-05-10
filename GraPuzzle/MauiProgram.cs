8abd23d43d663406e981da55a826658562bb3229
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:27:55 +0200

    Update MauiProgram.cs
    Dokumentacja

--- GraPuzzle/MauiProgram.cs
+++ GraPuzzle/MauiProgram.cs
@@ -1,27 +1,44 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using Plugin.Maui.Audio;
 
 namespace GraPuzzle
 {
+    // Klasa konfigurująca aplikację .NET MAUI
     public static class MauiProgram
     {
+        // Metoda tworząca aplikację MAUI
         public static MauiApp CreateMauiApp()
         {
+            // Tworzenie buildera aplikacji
             var builder = MauiApp.CreateBuilder();
+
             builder
+
+                // Ustawienie głównej klasy aplikacji
                 .UseMauiApp<App>()
+
+                // Konfiguracja czcionek
                 .ConfigureFonts(fonts =>
                 {
+                    // Dodanie podstawowej czcionki
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+
+                    // Dodanie pogrubionej czcionki
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
+            // Włączenie logowania w trybie debugowania
     		builder.Logging.AddDebug();
-#endif     
+#endif
+
+            // Rejestracja menedżera audio jako singleton
             builder.Services.AddSingleton(AudioManager.Current);
+
+            // Rejestracja strony MainPage
             builder.Services.AddTransient<MainPage>();
 
+            // Zbudowanie i zwrócenie aplikacji
             return builder.Build();
         }
     }
