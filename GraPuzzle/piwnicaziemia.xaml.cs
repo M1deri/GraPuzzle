69f8fa809ffef2f4560f7240c8cddee15a18a9aa
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:34:15 +0200

    Update piwnicaziemia.xaml.cs
    Dokumentacja

--- GraPuzzle/piwnicaziemia.xaml.cs
+++ GraPuzzle/piwnicaziemia.xaml.cs
@@ -1,29 +1,39 @@
 namespace GraPuzzle;
 
+// Logika sceny: piwnica (ziemia / podziemia)
 public partial class piwnicaziemia : ContentPage
 {
-	public piwnicaziemia()
-	{
-		InitializeComponent();
-	}
+    // Konstruktor strony
+    public piwnicaziemia()
+    {
+        InitializeComponent();
+    }
+
+    // Strzałka w lewo (funkcja nieużywana w XAML)
     private async void OnLeftArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new dzieckopokoj());
     }
 
+    // Strzałka w prawo (funkcja nieużywana w XAML)
     private async void OnRightArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Sypialnia());
     }
+
+    // Otwarcie ekwipunku
     private async void OnInventoryClicked(object sender, EventArgs e)
     {
         await Navigation.PushModalAsync(new InventoryPage());
     }
+
+    // Kliknięcie głównego obiektu – przejście do dalszej części piwnicy
     private async void Button_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushModalAsync(new piwnicaza());
     }
 
+    // Kliknięcie powrotu – przejście do wejścia piwnicy
     private async void Button_Clicked_1(object sender, EventArgs e)
     {
         await Navigation.PushModalAsync(new piwnica_wejscie());
