c922df4fcf0889f84a27c13c78c7ecbb47a60c26
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:23:19 +0200

    Update AppShell.xaml.cs
    Dokumentacja

--- GraPuzzle/AppShell.xaml.cs
+++ GraPuzzle/AppShell.xaml.cs
@@ -1,14 +1,22 @@
-﻿namespace GraPuzzle
+namespace GraPuzzle
 {
+    // Klasa odpowiedzialna za nawigację w aplikacji
     public partial class AppShell : Shell
     {
+        // Konstruktor AppShell
         public AppShell()
         {
+            // Inicjalizacja komponentów z AppShell.xaml
             InitializeComponent();
+
+            // Rejestracja trasy do strony Sypialnia
             Routing.RegisterRoute("sypialnia", typeof(Sypialnia));
+
+            // Rejestracja trasy do strony dzieckopokoj
             Routing.RegisterRoute("dzieckopokoj", typeof(dzieckopokoj));
+
+            // Rejestracja trasy do strony końcowej
             Routing.RegisterRoute("end", typeof(end));
         }
-
     }
 }
