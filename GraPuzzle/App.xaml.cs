6e5378f40cb6f9c1d9f124c869c3a5f7659534ae
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:22:28 +0200

    Update App.xaml.cs
    Dokumentacja

--- GraPuzzle/App.xaml.cs
+++ GraPuzzle/App.xaml.cs
@@ -1,14 +1,19 @@
-﻿namespace GraPuzzle
+namespace GraPuzzle
 {
+    // Główna klasa aplikacji
     public partial class App : Application
     {
+        // Konstruktor aplikacji
         public App()
         {
+            // Inicjalizacja komponentów z App.xaml
             InitializeComponent();
         }
 
+        // Metoda tworząca główne okno aplikacji
         protected override Window CreateWindow(IActivationState? activationState)
         {
+            // Utworzenie okna z główną nawigacją AppShell
             return new Window(new AppShell());
         }
     }
