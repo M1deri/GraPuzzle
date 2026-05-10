c728900120297676bc82ba757bae33d0291797cc
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:33:35 +0200

    Update piwnicaza.xaml.cs
    Dokumentacja

--- GraPuzzle/piwnicaza.xaml.cs
+++ GraPuzzle/piwnicaza.xaml.cs
@@ -1,36 +1,47 @@
 namespace GraPuzzle;
 
+// Logika sceny piwnicy (dalsza część)
 public partial class piwnicaza : ContentPage
 {
-	public piwnicaza()
-	{
-		InitializeComponent();
-	}
+    // Konstruktor strony
+    public piwnicaza()
+    {
+        InitializeComponent();
+    }
+
+    // Strzałka w lewo – (funkcja aktualnie niepodłączona w XAML)
     private async void OnLeftArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new dzieckopokoj());
     }
 
+    // Strzałka w prawo – (funkcja aktualnie niepodłączona w XAML)
     private async void OnRightArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Sypialnia());
     }
+
+    // Otwarcie ekwipunku
     private async void OnInventoryClicked(object sender, EventArgs e)
     {
         await Navigation.PushModalAsync(new InventoryPage());
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    // Kliknięcie głównego obiektu – przejście do Puzzle1
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-        Navigation.PushModalAsync(new Puzzle1());
+        await Navigation.PushModalAsync(new Puzzle1());
     }
 
-    private void Button_Clicked_1(object sender, EventArgs e)
+    // Kliknięcie obiektu (trumna) – zdobycie przedmiotu i informacji
+    private async void Button_Clicked_1(object sender, EventArgs e)
     {
-        DisplayAlert(
+        await DisplayAlert(
             "Sukces!",
-            "Trumna zosta�a otwarta.\nKryje si� w niej klucz w kszta�cie czaszki i\nKod 6767",
+            "Trumna została otwarta.\nKryje się w niej klucz w kształcie czaszki i\nKod 6767",
             "OK");
+
+        // Dodanie klucza do ekwipunku
         Inventory.Add("Skool Key");
     }
 }
