485a125530dcebef17083b3d9d7b3abc359e766e
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:24:20 +0200

    Update Inventory.cs
    Dokumentacja

--- GraPuzzle/Inventory.cs
+++ GraPuzzle/Inventory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace GraPuzzle
 {
+    // Klasa odpowiedzialna za przechowywanie przedmiotów gracza
     public class Inventory
     {
+        // Lista przedmiotów w ekwipunku
         public static List<string> Items = new();
+
+        // Metoda dodająca przedmiot do ekwipunku
         public static void Add(string item)
         {
             Items.Add(item);
         }
+
+        // Metoda sprawdzająca,
+        // czy dany przedmiot znajduje się w ekwipunku
         public static bool Has(string item)
         {
             return Items.Contains(item);
