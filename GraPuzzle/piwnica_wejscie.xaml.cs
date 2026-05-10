dae09297f4f9f43d861e8eac42eddc665a0292ef
MichauCrow <113516509+MichauCrow@users.noreply.github.com>
2026-05-10 18:32:54 +0200

    Update piwnica_wejscie.xaml.cs
    Dokumentacja

--- GraPuzzle/piwnica_wejscie.xaml.cs
+++ GraPuzzle/piwnica_wejscie.xaml.cs
@@ -1,27 +1,35 @@
 namespace GraPuzzle;
 
+// Logika wejścia do piwnicy
 public partial class piwnica_wejscie : ContentPage
 {
-	public piwnica_wejscie()
-	{
-		InitializeComponent();
-	}
+    // Konstruktor strony
+    public piwnica_wejscie()
+    {
+        InitializeComponent();
+    }
+
+    // Strzałka w lewo – przejście do pokoju dziecięcego
     private async void OnLeftArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new dzieckopokoj());
     }
 
+    // Strzałka w prawo – przejście do sypialni
     private async void OnRightArrowClicked3(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Sypialnia());
     }
+
+    // Otwarcie ekwipunku
     private async void OnInventoryClicked(object sender, EventArgs e)
     {
         await Navigation.PushModalAsync(new InventoryPage());
     }
 
+    // Kliknięcie ukrytego obiektu – wejście do podziemi
     private async void Button_Clicked(object sender, EventArgs e)
     {
-         await Navigation.PushModalAsync(new piwnicaziemia());
+        await Navigation.PushModalAsync(new piwnicaziemia());
     }
 }
